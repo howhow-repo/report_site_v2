4a5317c52b5a60d45c85799057f244181ec589aa
Howard Hsu <howard.hsu1129@gmail.com>
2021-11-24 17:43:54 +0800

    add channel

--- core/settings.py
+++ core/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
